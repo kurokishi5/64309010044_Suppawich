a7fcd99990d071382a21174d702c91e3391ee08a
idearsky <116155866+idearsky@users.noreply.github.com>
2022-11-17 09:29:24 +0700

    Create review3.js

---
+++ Week5/review3.js
@@ -0,0 +1,8 @@
+let obj = {
+    "studentID": 64309010044 ,
+    "name"     :"Suppawich",
+    "lastname" : "Tonzx",
+    "grade"    :3.45,
+    "Tel"      :[0934967237,0937592145]
+}
+console.log(obj.name,obj.grade,obj.Tel[0])
